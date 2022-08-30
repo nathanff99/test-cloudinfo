00ecf624b7b3fa12d4dceae88945b5cae31ae63c
Nathan Fischer <nathan@nathanfischer.dev.br>
2022-08-30 17:50:07 -0300

    feat: Add fake products json.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ class HomeController extends Controller
     public function index()
     {
         $banners = file_get_contents(storage_path('testing/json/banners.json'));
-        $products = [];
+        $products = file_get_contents(storage_path('testing/json/products.json'));
 
         return view('home.index', compact(
             'banners',
