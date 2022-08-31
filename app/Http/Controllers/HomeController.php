d12dadded78851b422f9d179778d59172de48f88
Nathan Fischer <nathan@nathanfischer.dev.br>
2022-08-31 19:38:44 -0300

    feat: Add partners in controller.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,9 +7,11 @@ class HomeController extends Controller
     public function index()
     {
         $banners = file_get_contents(storage_path('testing/json/banners.json'));
+        $partners = file_get_contents(storage_path('testing/json/partners.json'));
 
         return view('home.index', compact(
-            'banners'
+            'banners',
+            'partners'
         ));
     }
 }
